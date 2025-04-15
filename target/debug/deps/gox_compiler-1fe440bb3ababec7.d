0319632e077196e4161790b96e7f5acd1b11dcf0
CrisGO <cristhiangiraldo053@gmail.com>
2025-04-15 18:43:27 -0500

    git commit -m "Remove target folders from repo"

--- target/debug/deps/gox_compiler-1fe440bb3ababec7.d
+++
@@ -1,8 +0,0 @@
-/home/cris/Documents/Repo/gox-compiler/target/debug/deps/gox_compiler-1fe440bb3ababec7: src/main.rs src/lexer/mod.rs src/lexer/token_definitions.rs src/lexer/token.rs
-
-/home/cris/Documents/Repo/gox-compiler/target/debug/deps/gox_compiler-1fe440bb3ababec7.d: src/main.rs src/lexer/mod.rs src/lexer/token_definitions.rs src/lexer/token.rs
-
-src/main.rs:
-src/lexer/mod.rs:
-src/lexer/token_definitions.rs:
-src/lexer/token.rs:
--- target/debug/deps/gox_compiler-35265df19dc09097.d
+++
@@ -1,8 +0,0 @@
-/home/cris/Documents/Repo/gox-compiler/target/debug/deps/libgox_compiler-35265df19dc09097.rmeta: src/main.rs src/lexer/mod.rs src/lexer/token_definitions.rs src/lexer/token.rs
-
-/home/cris/Documents/Repo/gox-compiler/target/debug/deps/gox_compiler-35265df19dc09097.d: src/main.rs src/lexer/mod.rs src/lexer/token_definitions.rs src/lexer/token.rs
-
-src/main.rs:
-src/lexer/mod.rs:
-src/lexer/token_definitions.rs:
-src/lexer/token.rs:
